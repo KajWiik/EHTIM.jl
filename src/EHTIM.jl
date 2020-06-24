8cd8d9e82a32a9f5826033c624bb35b524d7ab3f
Kaj Wiik <kaj.wiik@utu.fi>
2020-06-24 11:45:07 +0300

    structarray() accepts now array of PythonObjects, bldict converts array to a Dict of baselines

--- src/EHTIM.jl
+++ src/EHTIM.jl
@@ -1,17 +1,40 @@
 module EHTIM
 using PyCall, StructArrays
 
-export structarray
+export structarray, bldict
 
 new() = pyimport("ehtim")
 
-function structarray(x)
+function structarray(x::PyCall.PyObject)
     labels = Symbol.(x.dtype.names)
     data = []
     for (i,n) in enumerate(labels)
         # Handle for Unicode arrays that are not supported by PyArray
+        # https://www.oreilly.com/library/view/python-for-finance/9781491945360/ch04.html
         push!(data, get(x.dtype, i-1).str[2] == 'U' ? get.(x, i-1) : PyArray(py"$x[$n]"o))
     end
     (;zip(labels, data)...)|>StructArray
 end
+
+function structarray(x::Array{PyCall.PyObject,1})
+    array = StructArray[]
+    for element in x
+        push!(array, structarray(element))
+    end
+    return array
+end
+
+function bldict(x::Array{StructArrays.StructArray,1})
+    dict = Dict{String,StructArray}()
+    for element in x
+        if hasproperty(element, :t1) && hasproperty(element, :t2)
+            dict[element.t1[1] > element.t2[1] ? element.t2[1]*"-"*element.t1[1] :
+                 element.t1[1]*"-"*element.t2[1]] = element
+        else
+            error("No baseline information in data.")
+        end
+    end
+    return dict
+end
+
 end # EHTIM
